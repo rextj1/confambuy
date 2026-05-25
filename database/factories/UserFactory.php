a9b3f9f9a825a046151db527cd6cd4e145cd0f6f
rextj1 <rextj121@gmail.com>
2026-05-25 12:54:08 +0100

    update user vendor seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,44 @@ class UserFactory extends Factory
 
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn () => [
             'email_verified_at' => null,
         ]);
     }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SUPER ADMIN STATE
+    |--------------------------------------------------------------------------
+    */
+    public function superAdmin(): static
+    {
+        return $this->state(fn () => [
+            'name' => config('super_admin.name'),
+            'email' => config('super_admin.email'),
+            'password' => Hash::make(config('super_admin.password')),
+            'phone' => 12345678923,
+            'is_active' => true,
+            'email_verified_at' => now(),
+        ]);
+    }
+
+    // /*
+    // |--------------------------------------------------------------------------
+    // | ADMIN STATE (OPTIONAL BUT USEFUL)
+    // |--------------------------------------------------------------------------
+    // */
+    // public function admin(): static
+    // {
+    //     return $this->state(fn () => [
+    //         'name' => config('admin.name', 'Admin User'),
+    //         'email' => config('admin.email', 'admin@example.com'),
+    //         'password' => Hash::make(config('admin.password', 'password')),
+    //         'phone' => 12345678923,
+    //         'is_active' => true,
+    //         'email_verified_at' => now(),
+    //     ]);
+    // }
+    
 }
+
